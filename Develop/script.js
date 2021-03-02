2f5a711daacf67b7d7e62218de1c47c7a1969088
Kristen Picard <kristenpicard93@gmail.com>
2021-03-02 16:41:24 -0500

    Got current weather working when search a city

--- Develop/script.js
+++ Develop/script.js
@@ -1,140 +1,67 @@
 var apiKey = "c979757ff3364fdbc7788b954c2541a8";
 
-
-// COPIED FROM AN ACTIVITY
-
-var resultTextEl = document.querySelector('#result-text');
-var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
-
-function getParams() {
-  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-  var searchParamsArr = document.location.search.split('&');
-
-  // Get the query and format values
-  var query = searchParamsArr[0].split('=').pop();
-
-  searchApi(query);
-}
-
-// function printResults(resultObj) {
-//   console.log(resultObj);
-
-//   // set up `<div>` to hold result content
-//   var resultCard = document.createElement('div');
-//   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-//   var resultBody = document.createElement('div');
-//   resultBody.classList.add('card-body');
-//   resultCard.append(resultBody);
-
-//   var titleEl = document.createElement('h3');
-//   titleEl.textContent = resultObj.title;
-
-//   var bodyContentEl = document.createElement('p');
-//   bodyContentEl.innerHTML =
-//     '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-
-//   if (resultObj.subject) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> No subject for this entry.';
-//   }
-
-//   if (resultObj.description) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong> ' + resultObj.description[0];
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong>  No description for this entry.';
-//   }
-
-//   var linkButtonEl = document.createElement('a');
-//   linkButtonEl.textContent = 'Read More';
-//   linkButtonEl.setAttribute('href', resultObj.url);
-//   linkButtonEl.classList.add('btn', 'btn-dark');
-
-//   resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
-//   resultContentEl.append(resultCard);
-// }
-
-function searchApi(query, format) {
-  var locQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
-
-  // Don't know how this is working
-//   locQueryUrl = locQueryUrl + '&q=' + query;
-
-  fetch(locQueryUrl)
-    .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
-
-      return response.json();
-    })
-    .then(function (locRes) {
-      // write query to page so user knows what they are viewing
-      resultTextEl.textContent = locRes.search.query;
-
-      console.log(locRes);
-
-      if (!locRes.results.length) {
-        console.log('No results found!');
-        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-      } else {
-        resultContentEl.textContent = '';
-        for (var i = 0; i < locRes.results.length; i++) {
-          printResults(locRes.results[i]);
-        }
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-}
+// var input = document.querySelector('#search-input');
+// var main = document.querySelector('#name');
+// var temp = document.querySelector('#temp');
+// var desc = document.querySelector('#desc');
+// var clouds = document.querySelector('#clouds');
+// var button= document.querySelector('#bttn');
 
 
+// button.addEventListener('click', function(){
+// fetch('api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid='+apiKey)
+// .then(response => response.json())
+// .then(data => {
+//   var tempValue = data['main']['temp'];
+//   var nameValue = data['name'];
+//   var descValue = data['weather'][0]['description'];
+
+//   main.innerHTML = nameValue;
+//   desc.innerHTML = "Desc - "+descValue;
+//   temp.innerHTML = "Temp - "+tempValue;
+//   input.value ="";
+
+// })
+
+// });
+
+  
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var date = document.querySelector('.date');
+var icon = document.querySelector('.icon');
+var temp = document.querySelector('.temp');
+var humidity = document.querySelector('.humidity');
+var wind = document.querySelector('.wind');
+var uvIndex = document.querySelector('.uv');
+var button= document.querySelector('.submit');
 
 
-var searchFormEl = document.querySelector('#search-form');
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid='+apiKey)
+.then(response => response.json())
+.then(data => {
+  
+  var nameValue = data.name;
+  var dateValue = data.dt;
+  // var iconName = data.weather[0].icon;
+  // var iconUrl = "http://openweathermap.org/img/w/" + iconName + ".png";  var tempValue = (((data.main.temp-273.15)* 1.8 +32).toFixed(0));
+  var tempValue = data.main.temp;
+  var humidValue = data.main.humidity;
+  var windValue = data.wind.speed;
+  // var uvValue = data.;
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-  var locQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
-  var searchInputVal = document.querySelector('#search-input').value;
+  main.innerHTML = nameValue; 
+  date.innerHTML = dateValue;
+  // icon.innerHTML = <img src=' '+ iconURL + ' '>";
+  temp.innerHTML = "Temperature: "+tempValue+" °F";
+  humidity.innerHTML = "Humidity: "+humidValue+" %";
+  wind.innerHTML = "Wind Speed: "+windValue+" MPH";
+  // uvIndex.innerHTML = 
+  
+  input.value ="";
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-// Need to update the following line with Weather API stuff!!!
-  var queryString = locQueryUrl + searchInputVal + apiKey;
+})});
 
-  location.assign(queryString);
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-
-
-
-
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-
-  searchApi(searchInputVal);
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-getParams();
+// .catch(err => alert("Wrong city name!"));
+// });
