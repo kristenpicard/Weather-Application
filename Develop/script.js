02f6f7f53cdd12c767a8226d7243fbe149bedfd2
Kristen Picard <kristenpicard93@gmail.com>
2021-03-05 19:08:09 -0500

    Separating Functions

--- Develop/script.js
+++ Develop/script.js
@@ -11,7 +11,7 @@ var uvIndex = document.querySelector('#uv');
 var button = document.querySelector('.submit');
 
 // City search button event listener
-button.addEventListener('click', function getCurrent (){
+button.addEventListener('click', function displayCityWeather (){
 
   // This fetch is for the current weather section
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid='+apiKey)
@@ -19,6 +19,19 @@ button.addEventListener('click', function getCurrent (){
   .then(data => {
   console.log(data);
 
+  // Get the latitude and longitude for the UV search from Open Weather Maps API
+  var latitude = data.coord.lat;
+  var longitude = data.coord.lon;
+
+  // Calling functions
+  displayCurrentWeather(data);
+  displayUVIndex(latitude, longitude);
+  displayFiveDayForecast(latitude, longitude);
+  
+  });
+});
+
+function displayCurrentWeather (data) {
   // These variables grab the correct data from the API
   var nameValue = data.name;
   var dateValue = moment().format('dddd, MMM DD, YYYY');
@@ -33,63 +46,52 @@ button.addEventListener('click', function getCurrent (){
   temp.innerHTML = "Temperature: "+tempValue+" °F";
   humidity.innerHTML = "Humidity: "+humidValue+"%";
   wind.innerHTML = "Wind Speed: "+windValue+" MPH";
+};
 
-  // Get the latitude and longitude for the UV search from Open Weather Maps API
-  var latitude = data.coord.lat;
-  var longitude = data.coord.lon;
+// Fetch the UV information and build the color coordination for the UV index
+function displayUVIndex (latitude, longitude) {  
   var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
-
-  // Fetch the UV information and build the color coordination for the UV index
   fetch(uvQueryURL)
-  .then(response => response.json())
-  .then(data => {
-      var uvIndexValue = data.value;
-      if (uvIndexValue >= 0 && uvIndexValue < 3){
-        uvIndex.innerHTML = `<span style="color:green">UV Index: ${uvIndexValue}</span>`;
-      } else if (uvIndexValue >= 3 && uvIndexValue < 8){
-        uvIndex.innerHTML = `<span style="color:orange">UV Index: ${uvIndexValue}</span>`;
-      } else if (uvIndexValue >= 8){
-        uvIndex.innerHTML = `<span style="color:red">UV Index: ${uvIndexValue}</span>`;
-      }
-  });
-
-  //Calling the function for storing city names
-  // searchHistory();
-
-  // Calling the function for the 5 day forecast so it displays at the time of this event listener also
-  fiveDayForecast(latitude, longitude);
-
-  });
-});
-
-// Begins work to build the 5-day forecast cards
-var cardDate1 = document.querySelector('.day-1-date');
-var cardIcon1 = document.querySelector('.day-1-icon');
-var cardTemp1 = document.querySelector('.day-1-temp');
-var cardHum1 = document.querySelector('.day-1-humidity');
-
-var cardDate2 = document.querySelector('.day-2-date');
-var cardIcon2 = document.querySelector('.day-2-icon');
-var cardTemp2 = document.querySelector('.day-2-temp');
-var cardHum2 = document.querySelector('.day-2-humidity');
-
-var cardDate3 = document.querySelector('.day-3-date');
-var cardIcon3 = document.querySelector('.day-3-icon');
-var cardTemp3 = document.querySelector('.day-3-temp');
-var cardHum3 = document.querySelector('.day-3-humidity');
-
-var cardDate4 = document.querySelector('.day-4-date');
-var cardIcon4 = document.querySelector('.day-4-icon');
-var cardTemp4 = document.querySelector('.day-4-temp');
-var cardHum4 = document.querySelector('.day-4-humidity');
-
-var cardDate5 = document.querySelector('.day-5-date');
-var cardIcon5 = document.querySelector('.day-5-icon');
-var cardTemp5 = document.querySelector('.day-5-temp');
-var cardHum5 = document.querySelector('.day-5-humidity');
+    .then(response => response.json())
+    .then(data => {
+        var uvIndexValue = data.value;
+        if (uvIndexValue >= 0 && uvIndexValue < 3){
+          uvIndex.innerHTML = `<span style="color:green">UV Index: ${uvIndexValue}</span>`;
+        } else if (uvIndexValue >= 3 && uvIndexValue < 8){
+          uvIndex.innerHTML = `<span style="color:orange">UV Index: ${uvIndexValue}</span>`;
+        } else if (uvIndexValue >= 8){
+          uvIndex.innerHTML = `<span style="color:red">UV Index: ${uvIndexValue}</span>`;
+        }
+    });
+};
 
 // 5-Day forecast function that is called above withint the search event listener
-function fiveDayForecast (latitude, longitude){
+function displayFiveDayForecast (latitude, longitude){
+  var cardDate1 = document.querySelector('.day-1-date');
+  var cardIcon1 = document.querySelector('.day-1-icon');
+  var cardTemp1 = document.querySelector('.day-1-temp');
+  var cardHum1 = document.querySelector('.day-1-humidity');
+
+  var cardDate2 = document.querySelector('.day-2-date');
+  var cardIcon2 = document.querySelector('.day-2-icon');
+  var cardTemp2 = document.querySelector('.day-2-temp');
+  var cardHum2 = document.querySelector('.day-2-humidity');
+
+  var cardDate3 = document.querySelector('.day-3-date');
+  var cardIcon3 = document.querySelector('.day-3-icon');
+  var cardTemp3 = document.querySelector('.day-3-temp');
+  var cardHum3 = document.querySelector('.day-3-humidity');
+
+  var cardDate4 = document.querySelector('.day-4-date');
+  var cardIcon4 = document.querySelector('.day-4-icon');
+  var cardTemp4 = document.querySelector('.day-4-temp');
+  var cardHum4 = document.querySelector('.day-4-humidity');
+
+  var cardDate5 = document.querySelector('.day-5-date');
+  var cardIcon5 = document.querySelector('.day-5-icon');
+  var cardTemp5 = document.querySelector('.day-5-temp');
+  var cardHum5 = document.querySelector('.day-5-humidity');
+
   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,hourly,minutely,alerts&units=imperial&appid=${apiKey}`)
   .then(response => response.json())
   .then(data => {
